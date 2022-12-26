7266bb341294d512c50e8b586bdc5483d0004321
Gergely Horváth <horvath@gergely.xyz>
2022-12-26 23:28:16 +0100

    Update .config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -174,7 +174,7 @@ lvim.builtin.treesitter.highlight.enable = true
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "~/.local/share/chezmoi/*",
-  command = "command!chezmoi apply"
+  command = "!chezmoi apply && notify-send updated"
 })
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   pattern = { "*.json", "*.jsonc" },
